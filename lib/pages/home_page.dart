de79a58aa6856fbae068ae0d0c16c42223f5225f
only4abhi <abhishekchanda.mailbox@gmail.com>
2022-06-27 08:44:05 +0530

    Git

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+
+class HomePage extends StatelessWidget {
+  int days = 30;
+  String name = "Codepur";
+  var myName = "Abhishek";
+  final pi = 3.14;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Container(
+          child: Center(
+              child: Text('My name is $myName. \nWelcome to $days days of Flutter.\nWelcome to '+name+'.')
+          ),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
