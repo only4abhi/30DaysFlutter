fc76a9d7388fe9bc88c1415a20ad1229525158b7
only4abhi <abhishekchanda.mailbox@gmail.com>
2022-06-30 21:28:04 +0530

    Day 13 Completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,15 +1,37 @@
+import 'dart:convert';
+
 import 'package:codepur/widgets/drawer.dart';
 import 'package:codepur/widgets/item_wisgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../models/catalog.dart';
 
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   int days = 30;
   String name = "Codepur";
   var myName = "Abhishek";
   final pi = 3.14;
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson =
+    await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    print(productsData);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +47,7 @@ class HomePage extends StatelessWidget {
           child: ListView.builder(
               itemCount:  dummyList.length,
               itemBuilder: (context,index){
-                  return ItemWidget(   key: null,item: dummyList[index]);
+                  return ItemWidget(  key: null,item: dummyList[index]);
               },
           ),
         ),
@@ -33,4 +55,8 @@ class HomePage extends StatelessWidget {
       drawer: MyDrawer(),
     );
   }
+
+
 }
+
+
